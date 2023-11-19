9c3c8b2e35a0ca52f8439995f748e3e81b12d7b3
Uzo <73608276+Uzo45@users.noreply.github.com>
2023-11-18 22:00:44 -0500

    Cleaning up some more

--- Assets/Pathfinding/Scripts/Pathfinding.cs
+++ Assets/Pathfinding/Scripts/Pathfinding.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:747a9c873c60d110be9a5bc008b9739a9e81730ea90d6050ae38dab611b1e65a
-size 14388
+oid sha256:7ae10b2d9a72ad5cbf97e53901da4795bb4948611e07b6e58aab21d3b632dc3e
+size 14708
